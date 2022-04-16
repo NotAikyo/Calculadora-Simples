f8999290457a9f175734d6139c40431cc3db27c6
NotAikyo <rafaelging77@gmail.com>
2022-04-16 17:49:19 -0300

    Trabalho

---
+++ Cálculos.py
@@ -0,0 +1,29 @@
+# Como fazer soma entre números 
+print('Para começar faça uma soma entre números!')
+primeiro_número = int(input('Digite um Número:  '))
+segundo_número = int(input('Digite Outro Número:  '))
+
+SOMATÓRIO = int(primeiro_número + segundo_número)
+
+print('A soma entre os números é: {} '.format(SOMATÓRIO))
+
+print('Agora faça uma multiplicação entre números!')
+
+# Como fazer multiplicação entre números
+terceiro_número = int(input('Digite um Número:  '))
+quarto_número = int(input('Digite Outro Número:  '))
+
+MULTIPLICAÇÃO = int(terceiro_número * quarto_número)
+
+print('A multiplicação entre os números é: {} '.format(MULTIPLICAÇÃO))
+
+print('Para finalizar faça uma divisão entre os números!')
+
+# Como Fazer divisão entre números
+quinto_número = int(input('Digite um Número:  '))
+sexto_número = int(input('Digite outro Número:  '))
+
+DIVISÃO = int(quinto_número / sexto_número)
+print('A divisão entre os números é: {} '.format(DIVISÃO))
+
+print('Com isso o programa está finalizado, até mais!')
